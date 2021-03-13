fb522963cd36e34c712df32a826ace822eca3c95
Randal Krebs <randal.t.krebs@gmail.com>
2021-03-13 18:02:52 -0300

    setUp

--- next.config.js
+++ next.config.js
@@ -12,4 +12,22 @@ module.exports = {
     CLOUD_NAME: "aguadeira",
     CLOUD_API: "https://api.cloudinary.com/v1_1/aguadeira/image/upload",
   },
+
+  serverRuntimeConfig: {
+    BASE_URL: "http://localhost:3000",
+    MONGODB_URL:
+      "mongodb+srv://randaltk:randaltk98@cluster0.z7cqv.mongodb.net/aguadeira?retryWrites=true&w=majority",
+    ACCESS_TOKEN_SECRET: "W4t:q.Rt%=}Vk$E72P?HjM>YvEG2<3T5Cu-,'n&`#K-V.Q.UYa",
+    REFRESH_TOKEN_SECRET:
+      "ug)[*uVjhZLuA7_#Tr.u*Xj;.z!pZ2E@@>Ad+xV%pbvq>/qK6{U%E-4$(mb_*J%3vZ-t.6u?'tzD]]@",
+    PAYPAL_CLIENT_ID:
+      "AUOCHPzTT21c6hvlrisZeWNvIoAQ_ONsDt_N0B4TRT74JNtCCKnV2URn6bwwc4u8s5izNTuY-B_EftHs",
+    CLOUD_UPDATE_PRESET: "aguadeira",
+    CLOUD_NAME: "aguadeira",
+    CLOUD_API: "https://api.cloudinary.com/v1_1/aguadeira/image/upload",
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    staticFolder: "/static",
+  },
 };
